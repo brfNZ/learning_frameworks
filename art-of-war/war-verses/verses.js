add38553d9659de735098ea9ed62c4f8c98f53b0
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 10:01:55 +1300

    Create main verses file for Art of War translations
    This file serves as the main verses file, combining translations and chapter titles for the Art of War.

---
+++ art-of-war/war-verses/verses.js
@@ -0,0 +1,24 @@
+// Main verses file - combines all Art of War translations
+// Load all translation files before this file
+
+var verseTexts = {
+    "Giles": versesGiles,
+    // "Griffith": versesGriffith,  // Uncomment when you add this translation
+};
+
+// Chapter titles for display
+var chapterTitles = [
+    "Laying Plans",
+    "Waging War",
+    "Attack by Stratagem",
+    "Tactical Dispositions",
+    "Energy",
+    "Weak Points and Strong",
+    "Maneuvering",
+    "Variation in Tactics",
+    "The Army on the March",
+    "Terrain",
+    "The Nine Situations",
+    "The Attack by Fire",
+    "The Use of Spies"
+];
