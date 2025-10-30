ea18151501cf66076db6f8fed93b0580291215b8
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 11:18:31 +1300

    Delete verses and lectures for entries 29 and 43
    Removed verses and lectures for entries 29 and 43 in verses-giles.js.

--- art-of-war/war-verses/verses-giles.js
+++ art-of-war/war-verses/verses-giles.js
@@ -22,41 +22,7 @@ passes; the chances of life and death.
 The Commander stands for the virtues of wisdom, sincerity, benevolence, courage and
 strictness.`,
     
-    "29": `Not doing
-
-Those who think to win the world
-by doing something to it,
-I see them come to grief.
-For the world is a sacred object.
-Nothing is to be done to it.
-To do anything to it is to damage it.
-To seize it is to lose it.
-
-Under heaven some things lead, some follow.
-some blow hot, some cold.
-some are strong, some weak.
-some are fulfilled, some fail.
-
-So the wise soul keeps away
-from the extremes, excess, extravagance.
-
----
-
-Lecture: For Lao Tzu, "moderation in all things" isn't just a bit of safe, practical advice. To lose the sense of the sacredness of the world is a mortal loss. To injure our world by excesses of greed and ingenuity is to endanger our own sacredness.`,
+    "29": ``,
     
-    "43": `Water and stone
-
-What's softest in the world
-rushes and runs
-over what's hardest in the world.
-
-The immaterial
-enters
-the impenetrable.
-
-So I know the good in not doing.
-
-The wordless teaching,
-the profit in not doing-
-not many people understand it.`,
+    "43": ``,
 };
