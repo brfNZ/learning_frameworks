fa389abd7c79bc835692cc29571fa76385f860cb
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 11:54:24 +1300

    Add Wing translation and chapter titles

--- art-of-war/war-verses/verses.js
+++ art-of-war/war-verses/verses.js
@@ -1,13 +1,12 @@
 // Main verses file - combines all Art of War translations
-// Load all translation files before this file
 
 var verseTexts = {
     "Giles": versesGiles,
-    // "Griffith": versesGriffith,  // Uncomment when you add this translation
+    "Wing": versesWing,
 };
 
-// Chapter titles for display
-var chapterTitles = [
+// Giles chapter titles
+var chapterTitlesGiles = [
     "Laying Plans",
     "Waging War",
     "Attack by Stratagem",
@@ -22,3 +21,18 @@ var chapterTitles = [
     "The Attack by Fire",
     "The Use of Spies"
 ];
+
+// Wing chapter titles (different translations)
+var chapterTitlesWing = [
+    "The Five Fundamentals of Strategy",  // Chapter 1
+    "Examining the Fundamentals",
+    "The Tao of Paradox",
+    "Foretelling Triumph",
+    " ",  // Chapter 2
+    // ... complete for all 13 chapters
+];
+
+// Helper to get the right title array
+function getChapterTitles(translation) {
+    return translation === "Wing" ? chapterTitlesWing : chapterTitlesGiles;
+}
