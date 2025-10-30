abd4a50a9ed5ba2f3e5cc633400d33b0d3e19819
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 07:36:24 +1300

    Add files via upload

---
+++ tao/verses/verses.js
@@ -0,0 +1,9 @@
+// Main verses file - combines all translations
+// Load all translation files before this file
+var verseTexts = {
+    "Lau": versesLau,
+    "Mitchell": versesMitchell,
+    "Cheng": versesCheng,
+    "Le Guin": versesLeGuin,
+    "Wing": versesWing,
+};
