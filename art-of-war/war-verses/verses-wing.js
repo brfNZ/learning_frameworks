3e96fbab156a460a2c312e42e4ef65aec3deadfe
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 11:32:11 +1300

    wing verse 1

---
+++ art-of-war/war-verses/verses-wing.js
@@ -0,0 +1,27 @@
+THE FIVE FUNDAMENTALS OF STRATEGY
+Sun Tzu said:
+Strategy is the great Work of the organization.
+In Situations of life or death, it is the Tao of survival or extinction.
+Its study cannot be neglected.
+Therefore, Calculate a plan with Five Working Fundamentals,
+And examine the condition of each.
+The first is Tao.
+The second is Nature.
+The third is Situation.
+The fourth is Leadership.
+The fifth is Art.
+The Tao inspires people to share in the same ideals and expectations.
+Hence, because they share in life and share in death,
+The people do not fear danger.
+Nature is the dark or light, the cold or hot, and the Systems of time.
+Situation is the distant or immediate, the obstructed or easy,
+The broad or narrow, and the chances of life or death.
+Leadership is intelligence, credibility, humanity, courage, and discipline.
+The Art is a flexible System
+Wherein the View and its officials employ the Tao.
+Leaders should not be unfamiliar with these Five.
+Those who understand them will triumph.
+Those who do not understand them will be defeated.
+The word View can also be translated as "master" or "sovereign," and refers to
+the mind, vision, or principle behind events.
+Modem Chinese sometimes use this ideogram to refer to Jesus.
