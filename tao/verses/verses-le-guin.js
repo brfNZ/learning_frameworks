7110a8741afd1fd7a8db22edf07d9012a40fa45d
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 09:38:05 +1300

    Refactor verses to use template literals

--- tao/verses/verses-le-guin.js
+++ tao/verses/verses-le-guin.js
@@ -1,6 +1,66 @@
 // Le Guin Translation
 var versesLeGuin = {
-    "23": "Nothing and not\n\nNature doesn't make long speeches.\nA whirlwind doesn't last all morning.\nA cloudburst doesn't last all day.\nWho makes the wind and rain?\nHeaven and earth do.\nIf heaven and earth don't go on and on,\ncertainly people don't need to.\n\nThe people who work with Tao\nare Tao people,\nthey belong to the Way.\nPeople who work with power\nbelong to power.\nPeople who work with loss\nbelong to what's lost.\n\nGive yourself to the Way\nand you'll be at home on the Way.\nGive yourself to power\nand you'll be at home in power.\nGive yourself to loss\nand when you're lost you'll be at home.\n\nTo give no trust\nis to get no trust.",
-    "29": "Not doing\n\nThose who think to win the world\nby doing something to it,\nI see them come to grief.\nFor the world is a sacred object.\nNothing is to be done to it.\nTo do anything to it is to damage it.\nTo seize it is to lose it.\n\nUnder heaven some things lead, some follow.\nsome blow hot, some cold.\nsome are strong, some weak.\nsome are fulfilled, some fail.\n\nSo the wise soul keeps away\nfrom the extremes, excess, extravagance.\n\n---\n\nLecture: For Lao Tzu, \"moderation in all things\" isn't just a bit of safe, practical advice. To lose the sense of the sacredness of the world is a mortal loss. To injure our world by excesses of greed and ingenuity is to endanger our own sacredness.",
-    "43": "Water and stone\n\nWhat's softest in the world\nrushes and runs\nover what's hardest in the world.\n\nThe immaterial\nenters\nthe impenetrable.\n\nSo I know the good in not doing.\n\nThe wordless teaching,\nthe profit in not doing-\nnot many people understand it.",
+    "23": `Nothing and not
+
+Nature doesn't make long speeches.
+A whirlwind doesn't last all morning.
+A cloudburst doesn't last all day.
+Who makes the wind and rain?
+Heaven and earth do.
+If heaven and earth don't go on and on,
+certainly people don't need to.
+
+The people who work with Tao
+are Tao people,
+they belong to the Way.
+People who work with power
+belong to power.
+People who work with loss
+belong to what's lost.
+
+Give yourself to the Way
+and you'll be at home on the Way.
+Give yourself to power
+and you'll be at home in power.
+Give yourself to loss
+and when you're lost you'll be at home.
+
+To give no trust
+is to get no trust.`,
+    "29": `Not doing
+
+Those who think to win the world
+by doing something to it,
+I see them come to grief.
+For the world is a sacred object.
+Nothing is to be done to it.
+To do anything to it is to damage it.
+To seize it is to lose it.
+
+Under heaven some things lead, some follow.
+some blow hot, some cold.
+some are strong, some weak.
+some are fulfilled, some fail.
+
+So the wise soul keeps away
+from the extremes, excess, extravagance.
+
+---
+
+Lecture: For Lao Tzu, "moderation in all things" isn't just a bit of safe, practical advice. To lose the sense of the sacredness of the world is a mortal loss. To injure our world by excesses of greed and ingenuity is to endanger our own sacredness.`,
+    "43": `Water and stone
+
+What's softest in the world
+rushes and runs
+over what's hardest in the world.
+
+The immaterial
+enters
+the impenetrable.
+
+So I know the good in not doing.
+
+The wordless teaching,
+the profit in not doing-
+not many people understand it.`,
 };
