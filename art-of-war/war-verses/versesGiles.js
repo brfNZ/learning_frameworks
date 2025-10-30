74142c291cc547465ff397adf9ad4995f9683869
brfNZ <moraine.doilies8o@icloud.com>
2025-10-31 09:46:59 +1300

    Add Le Guin translation of verses to versesGiles.js

---
+++ art-of-war/war-verses/versesGiles.js
@@ -0,0 +1,68 @@
+// Le Guin Translation
+var versesLeGuin = {
+    "23": `Nothing and not
+
+Nature doesn't make long speeches.
+A whirlwind doesn't last all morning.
+A cloudburst doesn't last all day.
+Who makes the wind and rain?
+Heaven and earth do.
+If heaven and earth don't go on and on,
+certainly people don't need to.
+
+The people who work with Tao
+are Tao people,
+they belong to the Way.
+People who work with power
+belong to power.
+People who work with loss
+belong to what's lost.
+
+Give yourself to the Way
+and you'll be at home on the Way.
+Give yourself to power
+and you'll be at home in power.
+Give yourself to loss
+and when you're lost you'll be at home.
+
+To give no trust
+is to get no trust.`,
+    
+    "29": `Not doing
+
+Those who think to win the world
+by doing something to it,
+I see them come to grief.
+For the world is a sacred object.
+Nothing is to be done to it.
+To do anything to it is to damage it.
+To seize it is to lose it.
+
+Under heaven some things lead, some follow.
+some blow hot, some cold.
+some are strong, some weak.
+some are fulfilled, some fail.
+
+So the wise soul keeps away
+from the extremes, excess, extravagance.
+
+---
+
+Lecture: For Lao Tzu, "moderation in all things" isn't just a bit of safe, practical advice. To lose the sense of the sacredness of the world is a mortal loss. To injure our world by excesses of greed and ingenuity is to endanger our own sacredness.`,
+    
+    "43": `Water and stone
+
+What's softest in the world
+rushes and runs
+over what's hardest in the world.
+
+The immaterial
+enters
+the impenetrable.
+
+So I know the good in not doing.
+
+The wordless teaching,
+the profit in not doing-
+not many people understand it.`,
+};
